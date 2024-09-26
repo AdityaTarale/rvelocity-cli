5d0a6fa847a9b0f28d8e246e37f1cf2d54c118c9
AdityaTarale <tarale.adi@gmail.com>
2024-09-26 23:41:17 +0530

    Refactor templates

--- commands/generate.js
+++ commands/generate.js
@@ -1,5 +1,10 @@
 const fs = require("fs");
 const path = require("path");
+const {
+  getComponentTemplate,
+  getStylesTemplate,
+  barrelTemplate,
+} = require("../templates/generate-file");
 
 module.exports = function (args) {
   const [folderName, componentName, platformFlag] = args;
@@ -13,6 +18,7 @@ module.exports = function (args) {
 
   const isReactNative = platformFlag === "-rn";
 
+  // Define paths for the component, styles, and barrel files
   const componentDir = path.join(
     process.cwd(),
     "src",
@@ -26,57 +32,22 @@ module.exports = function (args) {
   );
   const barrelFilePath = path.join(componentDir, "index.ts");
 
+  // Create the component directory if it doesn't exist
   if (!fs.existsSync(componentDir)) {
     fs.mkdirSync(componentDir, { recursive: true });
   }
 
-  const componentTemplate = `
-  import React from 'react';
-  ${
-    isReactNative
-      ? `import { View, Text } from 'react-native';\nimport styles from './styles';`
-      : `import './styles.css';`
-  }
+  // Get the appropriate templates for the component and styles
+  const componentTemplate = getComponentTemplate(componentName, isReactNative);
+  const stylesTemplate = getStylesTemplate(componentName, isReactNative);
+  const barrelTemplateContent = barrelTemplate(componentName);
 
-  type ${componentName}Props = {
-    // Define props here
-  };
-
-  const ${componentName}: React.FC<${componentName}Props> = (props) => {
-    return (
-      ${
-        isReactNative
-          ? `<View style={styles.container}><Text>${componentName} Component</Text></View>`
-          : `<div><h1>${componentName} Component</h1></div>`
-      }
-    );
-  };
-
-  export default ${componentName};
-  `;
-
-  const stylesTemplate = isReactNative
-    ? `import { StyleSheet } from 'react-native';
-
-  export default StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-  });
-  `
-    : `.${componentName.toLowerCase()} {
-    /* Add styles here */
-  }
-  `;
-
-  const barrelTemplate = `export { default } from './${componentName}';`;
-
+  // Write the files: component, styles, and barrel
   fs.writeFileSync(componentFilePath, componentTemplate, "utf8");
   fs.writeFileSync(styleFilePath, stylesTemplate, "utf8");
-  fs.writeFileSync(barrelFilePath, barrelTemplate, "utf8");
+  fs.writeFileSync(barrelFilePath, barrelTemplateContent, "utf8");
 
+  // Console log success message
   console.log(
     `${componentName} component for ${
       isReactNative ? "React Native" : "ReactJS"
---
+++ templates/reactNative/ReactNativeComponent.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { View, Text } from "react-native";
+import styles from "./styles";
+
+type __COMPONENT_NAME__Props = {
+  // Define props here
+};
+
+const __COMPONENT_NAME__: React.FC<__COMPONENT_NAME__Props> = (props) => {
+  return (
+    <View style={styles.container}>
+      <Text>__COMPONENT_NAME__ Component</Text>
+    </View>
+  );
+};
+
+export default __COMPONENT_NAME__;
