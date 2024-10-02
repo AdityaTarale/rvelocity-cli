37dfe8d9300603bc4ec09b8f6a7379e93f343827
AdityaTarale <tarale.adi@gmail.com>
2024-10-02 18:27:02 +0530

    Basic file structure

---
+++ rnuikit/src/ui/elements/TextInput/TextInput.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css";
+
+type TextInputProps = {
+  // Define props here
+};
+
+const TextInput: React.FC<TextInputProps> = (props) => {
+  return (
+    <div>
+      <h1>TextInput Component</h1>
+    </div>
+  );
+};
+
+export default TextInput;
