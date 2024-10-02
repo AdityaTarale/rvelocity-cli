43ab2dede058ed13ce386de037d3951d5a9a72ab
AdityaTarale <tarale.adi@gmail.com>
2024-10-02 22:29:56 +0530

    Cosmetic changes

--- bin/cli.js
+++ bin/cli.js
@@ -3,6 +3,19 @@ import { program } from "commander";
 import initCommand from "../commands/init.js";
 import generateCommand from "../commands/generate.js";
 import figlet from "figlet";
+import open from "open";
+import updateNotifier from "update-notifier";
+import { createRequire } from "module";
+
+// Dynamically import package.json using createRequire
+const require = createRequire(import.meta.url);
+const pkg = require("../package.json");
+
+const notifier = updateNotifier({ pkg });
+
+if (notifier.update) {
+  notifier.notify();
+}
 
 figlet("Rvelocity CLI", function (err, data) {
   if (err) {
@@ -28,6 +41,13 @@ program
   .action((folder, component, platform) => {
     const args = [folder, component, platform];
     generateCommand(args);
+  });
+
+program
+  .command("docs")
+  .description("Open CLI documentation")
+  .action(() => {
+    open("https://github.com/AdityaTarale/rvelocity-cli");
   });
 
 program.parse(process.argv);
