5ce9d1ed20c20bc86e41f93a9a5733b459da4ca1
AdityaTarale <tarale.adi@gmail.com>
2024-10-11 18:42:28 +0530

    Refactor avatar story

--- rnuikit/src/ui/elements/media-icons/Avatar/Avatar.tsx
+++ rnuikit/src/ui/elements/media-icons/Avatar/Avatar.tsx
@@ -1,3 +1,4 @@
+import { Theme } from '@/theme';
 import React, {
   Children,
   cloneElement,
@@ -9,50 +10,22 @@ import React, {
   useMemo,
   useState,
 } from 'react';
-import { ImageProps, StyleProp, View, ViewStyle } from 'react-native';
+import { ImageProps, View } from 'react-native';
 import { useStyles } from 'react-native-unistyles';
+import Text from '../../Text';
 import { AutoImage } from '../Image';
 import stylesheet from './styles';
-import Text from '../../Text';
-import { Theme } from '@/theme';
-
-const avatarVariants = {
-  xs: {
-    width: 24,
-    height: 24,
-  },
-  sm: {
-    width: 30,
-    height: 30,
-  },
-  md: {
-    width: 50,
-    height: 50,
-  },
-  lg: {
-    width: 64,
-    height: 64,
-  },
-  xl: {
-    width: 96,
-    height: 96,
-  },
-  '2xl': {
-    width: 134,
-    height: 134,
-  },
-};
 
 const avatarText = {
   xs: 'bodySmall',
   sm: 'bodyMedium',
   md: 'bodyLarge',
-  lg: 'displaySmall',
-  xl: 'displayMedium',
-  '2xl': 'displayLarge',
+  lg: 'bodyLarge',
+  xl: 'bodyLarge',
+  '2xl': 'bodyLarge',
 };
 
-type AvatarType = Exclude<keyof typeof avatarVariants, 'defaults'>;
+type AvatarType = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl';
 type AvatarGroupProps = {
   children: ReactElement[];
   max: number;
@@ -64,7 +37,6 @@ type AvatarContextType = {
   variant: AvatarType;
 };
 type AvatarProps = {
-  bg?: keyof Theme['colors'];
   variant?: AvatarType;
   children: ReactNode;
 };
@@ -72,7 +44,6 @@ type AvatarProps = {
 //   title: string;
 // };
 
-// Context
 const AvatarContext = createContext<AvatarContextType>({
   variant: 'md',
 });
@@ -83,7 +54,7 @@ const getAvatarGroupChildren = (
   children: ReactNode,
   max: number,
   variant: AvatarType,
-  styles: { plusAvatar: StyleProp<ViewStyle> },
+  styles: any,
 ): [ReactElement[], ReactElement | null] => {
   let childrenArray = Children.toArray(children) as ReactElement[];
   let plusAvatars = 0;
@@ -101,7 +72,7 @@ const getAvatarGroupChildren = (
 
   const plusAvatarComponent =
     plusAvatars > 0 ? (
-      <View key="plus-avatars" style={[styles.plusAvatar, avatarVariants[variant]]}>
+      <View key="plus-avatars" style={[styles.plusAvatar, styles[variant]]}>
         <Text color="background" variant={avatarText[variant] as keyof Theme['typeFaces']}>
           +{plusAvatars}
         </Text>
@@ -112,14 +83,17 @@ const getAvatarGroupChildren = (
 };
 
 const AvatarGroup = ({ children, max, variant = 'md' }: AvatarGroupProps): ReactElement => {
+  const memorizedValue = useMemo(() => ({ variant }), [variant]);
   const { styles } = useStyles(stylesheet);
   const [avatars, plusAvatarComponent] = getAvatarGroupChildren(children, max, variant, styles);
 
   return (
-    <View style={styles.groupContainer}>
-      <View style={styles.groupContainer}>{avatars}</View>
-      {plusAvatarComponent}
-    </View>
+    <Provider value={memorizedValue}>
+      <View style={styles.groupContainer}>
+        <View style={styles.groupContainer}>{avatars}</View>
+        {plusAvatarComponent}
+      </View>
+    </Provider>
   );
 };
 
@@ -136,15 +110,15 @@ const AvatarImage = ({ source, ...props }: AutoImageProps): ReactElement => {
     <View
       style={[
         styles.avatarImage,
-        { width: avatarVariants[variant].width, height: avatarVariants[variant].height },
+        { width: styles[variant].width, height: styles[variant].height },
       ]}>
       {!imageError ? (
         <AutoImage
           source={source}
           onError={handleImageError}
           {...props}
-          maxWidth={avatarVariants[variant].width}
-          // maxHeight={avatarVariants[variant].height}
+          maxWidth={styles[variant].width}
+          // maxHeight={styles[variant].height}
         />
       ) : null}
     </View>
@@ -153,7 +127,6 @@ const AvatarImage = ({ source, ...props }: AutoImageProps): ReactElement => {
 
 const AvatarFallback = ({ children }: AvatarFallbackProps): ReactElement => {
   const { variant } = useContext(AvatarContext);
-  const { styles } = useStyles(stylesheet);
 
   const fullName = children?.toString().split(' ') ?? [];
   const firstNameInitial = fullName[0]?.length > 0 ? fullName[0][0] : '';
@@ -162,19 +135,10 @@ const AvatarFallback = ({ children }: AvatarFallbackProps): ReactElement => {
   const avatarTextVariant = avatarText[variant] as keyof Theme['typeFaces'];
 
   return (
-    <View
-      style={[
-        styles.avatarFallback,
-        {
-          width: avatarVariants[variant].width,
-          height: avatarVariants[variant].height,
-        },
-      ]}>
-      <Text variant={avatarTextVariant}>
-        {firstNameInitial}
-        {lastNameInitial}
-      </Text>
-    </View>
+    <Text variant={avatarTextVariant}>
+      {firstNameInitial}
+      {lastNameInitial}
+    </Text>
   );
 };
 
@@ -182,24 +146,18 @@ const AvatarFallback = ({ children }: AvatarFallbackProps): ReactElement => {
 //   return <View/>;
 // };
 
-const Avatar = ({ children, bg = 'warning', variant = 'md' }: AvatarProps): ReactElement => {
-  const memorizedValue = useMemo(() => ({ variant }), [variant]);
+const Avatar = ({ children, variant = 'md' }: AvatarProps): ReactElement => {
+  const { variant: groupVariant } = useContext(AvatarContext);
+
+  const memorizedValue = useMemo(
+    () => ({ variant: groupVariant || variant }),
+    [groupVariant, variant],
+  );
   const { styles } = useStyles(stylesheet);
 
   return (
     <Provider value={memorizedValue}>
-      <View
-        style={[
-          styles.avatarContainer,
-          {
-            backgroundColor: bg,
-            width: avatarVariants[variant].width,
-            height: avatarVariants[variant].height,
-            borderRadius: avatarVariants[variant].height / 2,
-          },
-        ]}>
-        {children}
-      </View>
+      <View style={[styles.avatarContainer, styles[groupVariant || variant]]}>{children}</View>
     </Provider>
   );
 };
@@ -210,19 +168,32 @@ Avatar.Fallback = AvatarFallback;
 
 export default Avatar;
 
-/* Usage Example:
-<Avatar.Group max={2}>
-  <Avatar>
-    <Avatar.Image source="https://example.com/image1.jpg" />
-    <Avatar.Fallback>John Doe</Avatar.Fallback>
-  </Avatar>
-  <Avatar>
-    <Avatar.Image source="https://example.com/image2.jpg" />
-    <Avatar.Fallback>Jane Smith</Avatar.Fallback>
-  </Avatar>
-  <Avatar>
-    <Avatar.Image source="https://example.com/image3.jpg" />
-    <Avatar.Fallback>Jim Brown</Avatar.Fallback>
-  </Avatar>
-</Avatar.Group>
-*/
+/**
+ * How to use
+ * <Avatar.Group max={2}>
+ *   <Avatar>
+ *     <Avatar.Image
+ *       source={{
+ *         uri: 'https://images.unsplash.com/photo-1614289371518-722f2615943d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+ *       }}
+ *     />
+ *     <Avatar.Fallback>Avatar</Avatar.Fallback>
+ *   </Avatar>
+ *   <Avatar>
+ *     <Avatar.Image
+ *       source={{
+ *         uri: 'https://images.unsplash.com/photo-1614289371518-722f2615943d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+ *       }}
+ *     />
+ *     <Avatar.Fallback>Avatar</Avatar.Fallback>
+ *   </Avatar>
+ *   <Avatar>
+ *     <Avatar.Image
+ *       source={{
+ *         uri: 'https://images.unsplash.com/photo-1614289371518-722f2615943d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'
+ *       }}
+ *     />
+ *     <Avatar.Fallback>Avatar</Avatar.Fallback>
+ *   </Avatar>
+ * </Avatar.Group>
+ */
--- rnuikit/src/ui/elements/media-icons/Avatar/styles.ts
+++ rnuikit/src/ui/elements/media-icons/Avatar/styles.ts
@@ -6,7 +6,7 @@ const stylesheet = createStyleSheet(theme => ({
     overflow: 'hidden',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: theme.colors.primary,
+    backgroundColor: theme.colors.darkGray,
   },
   groupContainer: {
     flexDirection: 'row',
@@ -18,16 +18,45 @@ const stylesheet = createStyleSheet(theme => ({
     justifyContent: 'center',
     alignItems: 'center',
     overflow: 'hidden',
-  },
-  avatarFallback: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    zIndex: 10,
   },
   avatarContainer: {
     justifyContent: 'center',
     alignItems: 'center',
     position: 'relative',
     overflow: 'hidden',
+    backgroundColor: theme.colors.secondary,
+  },
+  // Avatar size
+  xs: {
+    width: 24,
+    height: 24,
+    borderRadius: 12,
+  },
+  sm: {
+    width: 30,
+    height: 30,
+    borderRadius: 15,
+  },
+  md: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+  lg: {
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+  },
+  xl: {
+    width: 96,
+    height: 96,
+    borderRadius: 48,
+  },
+  '2xl': {
+    width: 134,
+    height: 134,
+    borderRadius: 67,
   },
 }));
 
